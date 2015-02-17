8714fdc1c912a91d8b213cb02460e11f18a436dc
Maxim <atomicid@outlook.com>
2015-02-16 20:27:46 -0800

    Initial commit

---
+++ src/test/java/core/Title_Validation_E2E_EndToEnd/AllTests.java
@@ -0,0 +1,17 @@
+package core.Title_Validation_E2E_EndToEnd;
+
+import org.junit.runner.RunWith;
+
+import org.junit.runners.Suite;
+
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({SeleniumTestParametersArray.class})
+public class AllTests {
+
+
+ 
+
+
+}
